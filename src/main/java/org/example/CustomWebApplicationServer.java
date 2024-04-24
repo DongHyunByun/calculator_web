d20baa1a7d11634fedb4ec7a3a452dff587299be
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 <ngoodsamari@naver.com>
2024-04-24 12:25:21 +0900

    final commit

--- src/main/java/org/example/CustomWebApplicationServer.java
+++ src/main/java/org/example/CustomWebApplicationServer.java
@@ -9,10 +9,12 @@ import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class CustomWebApplicationServer {
     private final int port;
-
+    private final ExecutorService executorService = Executors.newFixedThreadPool(10); //스레드풀을 이용
     private static final Logger logger = LoggerFactory.getLogger(CustomWebApplicationServer.class);
 
     public CustomWebApplicationServer(int port) {
@@ -29,7 +31,9 @@ public class CustomWebApplicationServer {
             while ((clientSocket = serverSocket.accept()) != null) {
                 logger.info("[CustomWebApplicationServer] client connected!"); // 클라이언트 연결(http파일 실행시 연결됨)
 
-                new Thread(new ClientRequesthandler(clientSocket)).start();
+//                new Thread(new ClientRequesthandler(clientSocket)).start(); // 연결이 있을때마다 스레드 객체를 생성하여 메모리상 비효율적이므로 아래와같이 스레드풀을 생성.
+                executorService.execute(new ClientRequesthandler(clientSocket)); //미리 스레드를 연결해놓는것
+
 
             }
         }
